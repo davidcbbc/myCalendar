0cae2ec473b977dd78000b8ab6678fb2dc7c79eb
davidcbbc <david.bc2.24@gmail.com>
2019-11-16 13:24:45 +0000

    Added some features

--- mycalendar/lib/main.dart
+++ mycalendar/lib/main.dart
@@ -27,56 +27,90 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 //TEMPLATE APP
-DateTime _selectedDay = DateTime.now();
-
-
+  DateTime _selectedDay = DateTime.now();
   @override
   Widget build(BuildContext context) {
+    String title = _selectedDay.day.toString();
+    title += " - ";
+    title += _selectedDay.month.toString();
+    title += " - ";
+    title += _selectedDay.year.toString();
 
     final card = Container(
+      padding: EdgeInsets.all(10),
+      height: 100,
+      width: 300,
       child: Card(
-        child: Text("Evento 1"),
+        color: Colors.grey[400],
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Row(children: <Widget>[Icon(Icons.people,color: Colors.grey[800],),Text("0")],),
+              Text("Evento", style: TextStyle(fontWeight: FontWeight.bold),),
+             IconButton(
+              icon: Icon(Icons.group_add,color: Colors.grey[800]),
+              onPressed: () {
+                // Adicionar um funcionario
+              },
+             ),
+            ],
+          ),
+        ),
       ),
     );
 
     return new Scaffold(
-      bottomNavigationBar:Container(
-        color: Colors.grey,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.email,color: Colors.white,),
-              onPressed: (){},
-            ),
-            IconButton(
-              icon: Icon(Icons.sms,color: Colors.white),
-              onPressed: (){},
-            ),
-          ],
+        bottomNavigationBar: Container(
+          color: Colors.grey[800],
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(
+                  Icons.email,
+                  color: Colors.white,
+                ),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.sms, color: Colors.white),
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
-      ),
-      appBar: new AppBar(
-        title: Text("Calendar Test"),
-        backgroundColor: Colors.grey,
-      ),
-      body: Column(
-        children: <Widget>[
-          DatePickerTimeline(
-            _selectedDay,
-            locale: "pt_BR",
-            onDateChange: (date) {
-              // New date selected
-              setState(() {
-                _selectedDay = date;
-              });
-            },
-          ),SingleChildScrollView(
-            child: card,
-          )
-        ],
-      ),
-    );
+        appBar: new AppBar(
+          title: Text(title),
+          backgroundColor: Colors.grey[800],
+        ),
+        body: ListView(
+          children: <Widget>[
+            DatePickerTimeline(
+              _selectedDay,
+              locale: "pt_BR",
+              onDateChange: (date) {
+                // New date selected
+                setState(() {
+                  _selectedDay = date;
+                });
+              },
+            ),
+            card,
+            card,
+            card,
+            card,
+            card,
+            card,
+            card,
+            card,
+            card,
+            card,
+            card,
+            card,
+            card,
+            card,
+          ],
+        ));
   }
-
 }
