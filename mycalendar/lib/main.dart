1dee4e90143f665da2c883ee6f81fc2ac7217be8
davidcbbc <david.bc2.24@gmail.com>
2019-11-18 22:58:27 +0000

    Added new widgets and gridview

--- mycalendar/lib/main.dart
+++ mycalendar/lib/main.dart
@@ -36,81 +36,126 @@ class _MyHomePageState extends State<MyHomePage> {
     title += " - ";
     title += _selectedDay.year.toString();
 
-    final card = Container(
-      padding: EdgeInsets.all(10),
-      height: 100,
-      width: 300,
-      child: Card(
-        color: Colors.grey[400],
-        child: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Row(children: <Widget>[Icon(Icons.people,color: Colors.grey[800],),Text("0")],),
-              Text("Evento", style: TextStyle(fontWeight: FontWeight.bold),),
-             IconButton(
-              icon: Icon(Icons.group_add,color: Colors.grey[800]),
-              onPressed: () {
-                // Adicionar um funcionario
-              },
-             ),
-            ],
+    final card = DragTarget(
+      builder: (context, List<String> candidateData, rejectedData) {
+        return Container(
+          padding: EdgeInsets.all(10),
+          height: 80,
+          width: 100,
+          child: Card( 
+            color: Colors.grey[400],
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.people,
+                        color: Colors.grey[800],
+                      ),
+                      Text("0")
+                    ],
+                  ),
+                  Text(
+                    "Evento",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.group_add, color: Colors.grey[800]),
+                    onPressed: () {
+                      // Adicionar um funcionario
+                    },
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
+      onWillAccept: (data) {
+        print("onWillAccept: $data");
+      },
+      onAccept: (data) {
+        print("1");
+        print("onAccept: $data");
+      },
+
+      onLeave: (data) {
+        print("2");
+        print("onLeaveL $data");
+      },
     );
 
     return new Scaffold(
-        bottomNavigationBar: Container(
-          color: Colors.grey[800],
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(
-                  Icons.email,
-                  color: Colors.white,
-                ),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(Icons.sms, color: Colors.white),
-                onPressed: () {},
-              ),
-            ],
-          ),
-        ),
-        appBar: new AppBar(
-          title: Text(title),
-          backgroundColor: Colors.grey[800],
-        ),
-        body: ListView(
+      bottomNavigationBar: Container(
+        color: Colors.grey[800],
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            DatePickerTimeline(
-              _selectedDay,
-              locale: "pt_BR",
-              onDateChange: (date) {
-                // New date selected
-                setState(() {
-                  _selectedDay = date;
-                });
-              },
+            IconButton(
+              icon: Icon(
+                Icons.email,
+                color: Colors.white,
+              ),
+              onPressed: () {},
             ),
-            card,
-            card,
-            card,
-            card,
-            card,
-            card,
-            card,
-            card,
-            card,
-            card,
-            card,
-            card,
-            card,
-            card,
+            IconButton(
+              icon: Icon(Icons.sms, color: Colors.white),
+              onPressed: () {},
+            ),
           ],
-        ));
+        ),
+      ),
+      appBar: new AppBar(
+        title: Text(title),
+        backgroundColor: Colors.grey[800],
+      ),
+      body: Column(
+        children: <Widget>[
+          DatePickerTimeline(
+            _selectedDay,
+            locale: "pt_BR",
+            onDateChange: (date) {
+              // New date selected
+              setState(() {
+                _selectedDay = date;
+              });
+            },
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Draggable<String>(
+                  data: "A TUA PRIMA OH BELHOTE",
+                  childWhenDragging: Icon(Icons.ac_unit,color: Colors.grey,),
+                  child: Container(
+                    child: Icon(Icons.ac_unit),
+                  ),
+                  feedback: Icon(Icons.ac_unit),
+
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: GridView.count(
+                      physics: ScrollPhysics(),
+                      shrinkWrap: true,
+                      crossAxisCount: 2,
+                      children: <Widget>[
+                        card,
+                        card
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
