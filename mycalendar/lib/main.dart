b78e946efc7edf48a354804564ff65a334d37851
davidcbbc <david.bc2.24@gmail.com>
2019-11-27 23:55:20 +0000

    Added drawer and updated DateTimePicker location

--- mycalendar/lib/main.dart
+++ mycalendar/lib/main.dart
@@ -36,25 +36,32 @@ class _MyHomePageState extends State<MyHomePage> {
     title += "-";
     title += _selectedDay.year.toString();
 
-
     final drag = Draggable<String>(
-                  data: "A TUA PRIMA OH BELHOTE",
-                  childWhenDragging: Container(
-                    child: Text("empregado",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.grey),textAlign: TextAlign.center,),
-                  ),
-                  child: Container(
-                    child: Text("empregado",style: TextStyle(fontWeight: FontWeight.bold),textAlign: TextAlign.center,),
-                  ),
-                  feedback: Material(
-                    child: Container(
-                      color: Colors.grey,
-                      child: Text("empregado",style: TextStyle(fontWeight: FontWeight.bold),textAlign: TextAlign.center,),
-                    ),
-                  )
-                );
-
-
-
+        data: "A TUA PRIMA OH BELHOTE",
+        childWhenDragging: Container(
+          child: Text(
+            "empregado",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        child: Container(
+          child: Text(
+            "empregado",
+            style: TextStyle(fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        feedback: Material(
+          child: Container(
+            color: Colors.grey,
+            child: Text(
+              "empregado",
+              style: TextStyle(fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ));
 
     final card = DragTarget(
       builder: (context, List<String> candidateData, rejectedData) {
@@ -62,28 +69,27 @@ class _MyHomePageState extends State<MyHomePage> {
           padding: EdgeInsets.all(10),
           height: 80,
           width: 100,
-          child: Card( 
-            color: Colors.grey[400],
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    "Evento",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(
-                        Icons.people,
-                        color: Colors.grey[800],
-                      ),
-                      Text("0")
-                    ],
-                  ),  
-                ],
-              ),
-            
+          child: Card(
+            color: Colors.grey[300],
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Evento",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.people,
+                      color: Colors.grey[800],
+                    ),
+                    Text("0")
+                  ],
+                ),
+              ],
+            ),
           ),
         );
       },
@@ -95,7 +101,6 @@ class _MyHomePageState extends State<MyHomePage> {
         print("1");
         print("onAccept: $data");
       },
-
       onLeave: (data) {
         print("2");
         print("onLeaveL $data");
@@ -103,121 +108,184 @@ class _MyHomePageState extends State<MyHomePage> {
     );
 
     return new Scaffold(
-      bottomNavigationBar: Container(
-        color: Colors.grey[800],
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.email,
-                color: Colors.white,
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              DrawerHeader(
+                child: Center(
+                  child: CircleAvatar(
+                    child: Icon(
+                      Icons.perm_contact_calendar,
+                      size: 60,
+                      color: Colors.white,
+                    ),
+                    radius: 50.0,
+                    backgroundColor: Colors.grey[500],
+                  ),
+                ),
+                decoration: BoxDecoration(color: Colors.grey[300]),
               ),
+              ListTile(
+                title: Text("Estatísticas"),
+                leading: Icon(Icons.insert_chart),
+                onTap: () {
+                  // Abrir aba das estatisticas
+                },
+              ),
+              ListTile(
+                title: Text("Definições"),
+                leading: Icon(Icons.settings),
+                onTap: () {
+                  // abrir aba das definicoes
+                },
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: Container(
+          color: Colors.grey[800],
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(
+                  Icons.email,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  // email para clientes
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.sms, color: Colors.white),
+                onPressed: () {
+                  // Sms para funcionarios
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.mail_outline, color: Colors.white),
+                onPressed: () {
+                  // Email para multipessoal
+                },
+              ),
+            ],
+          ),
+        ),
+        appBar: new AppBar(
+          title: Row(
+            children: <Widget>[
+              Text(title),
+              SizedBox(
+                width: 10,
+              ),
+              Icon(
+                Icons.group,
+                color: Colors.grey,
+              ),
+              Text("22")
+            ],
+          ),
+          backgroundColor: Colors.grey[800],
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add),
               onPressed: () {
-                // email para clientes
-                
+                // Adicionar evento
               },
             ),
             IconButton(
-              icon: Icon(Icons.sms, color: Colors.white),
+              icon: Icon(Icons.person_add),
               onPressed: () {
-                // Sms para funcionarios
+                // Adicionar funcionario
               },
             ),
             IconButton(
-              icon: Icon(Icons.mail_outline, color: Colors.white),
+              icon: Icon(Icons.business_center),
               onPressed: () {
-                // Email para multipessoal
+                // Adicionar cliente
               },
             ),
           ],
         ),
-      ),
-      appBar: new AppBar(
-        title: Row(children: <Widget>[Text(title),SizedBox(width: 10,),Icon(Icons.group,color: Colors.grey,),Text("22")],),
-        backgroundColor: Colors.grey[800],
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {
-              // Adicionar evento
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.person_add),
-            onPressed: () {
-              // Adicionar funcionario
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.business_center),
-            onPressed: () {
-              // Adicionar cliente
-            },
-          ),
-
-          ],
-      ),
-      body: Column(
-        children: <Widget>[
-          DatePickerTimeline(
-            _selectedDay,
-            locale: "pt_BR",
-            onDateChange: (date) {
-              // New date selected
-              setState(() {
-                _selectedDay = date;
-              });
-            },
-          ),
-          Expanded(
-            child: Row(
-              children: <Widget>[
-                Container(
-                  
-                  width: 150,
-                  color: Colors.grey[200],
-                  child: ListView(
-                    children: <Widget>[
-                      Text("DISP. TOTAL", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green),textAlign: TextAlign.center,),
-                      drag,
-                      drag,
-                      Text("DISP. REDUZIDA", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.orange),textAlign: TextAlign.center,),
-                      drag,
-                      Text("DISP. OCASIONAL", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey),textAlign: TextAlign.center,),
-                      drag,
-                    
-
-                    ],
-                  )
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Expanded(
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: GridView.count(
-                      scrollDirection: Axis.horizontal,
-                      physics: ScrollPhysics(),
-                      shrinkWrap: true,
-                      crossAxisCount: 2,
-                      children: <Widget>[
-                        card,
-                        card,
-                        card,
-                        card,
-                        card
-                        
-                      ],
+        body: Row(
+          children: <Widget>[
+            Container(
+                width: 150,
+                color: Colors.grey[200],
+                child: ListView(
+                  children: <Widget>[
+                    SizedBox(height: 20,),
+                    Text(
+                      "DISPONIBILIDADE TOTAL",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.green),
+                      textAlign: TextAlign.center,
                     ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    drag,
+                    SizedBox(
+                      height: 5,
+                    ),
+                    drag,
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      "DISPONIBILIDADE REDUZIDA",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.orange),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    drag,
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      "DISPONIBILIDADE OCASIONAL",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.grey),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    drag,
+                  ],
+                )),
+            Expanded(
+              child: Column(
+                children: <Widget>[
+                  DatePickerTimeline(
+                    _selectedDay,
+                    locale: "pt_BR",
+                    onDateChange: (date) {
+                      // New date selected
+                      setState(() {
+                        _selectedDay = date;
+                      });
+                    },
                   ),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+                  Expanded(
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: GridView.count(
+                        scrollDirection: Axis.horizontal,
+                        physics: ScrollPhysics(),
+                        shrinkWrap: true,
+                        crossAxisCount: 2,
+                        children: <Widget>[card, card, card, card, card],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
